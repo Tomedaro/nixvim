2640226fa99a3687b95ea128775a6dffebbcf788
Tomedaro <168759043+Tomedaro@users.noreply.github.com>
2025-05-29 19:57:02 +0200

    Another attempt

--- modules/hardtime/default.nix
+++ modules/hardtime/default.nix
@@ -1,26 +1,13 @@
-# /home/daniil/NixOS/modules/hardtime/default.nix (MINIMAL TEST)
-{ lib, pkgs, config, ... }: # config here should be the programs.nixvim scope
+# In your nixvim configuration (e.g., ~/.config/nixvim/plugins.nix or similar)
+{ pkgs, ... }:
 
-let
-  # This refers to the 'enable' option defined below, which you'll set in your main config.
-  cfg = config.programs.nixvim.plugins.hardtime;
-in
 {
-  # 1. Define a simple enable option for this test module
-  options.programs.nixvim.plugins.hardtime = {
-    enable = lib.mkEnableOption "Enable minimal hardtime plugin test";
-  };
-
-  # 2. If enabled, try to do only two very basic things:
-  #    - Add the plugin package.
-  #    - Set a universal, core nixvim option.
-  config = lib.mkIf cfg.enable {
-    programs.nixvim.extraPlugins = [
-      pkgs.vimPlugins.hardtime-nvim
-    ];
-
-    # Let's try to set one of the most basic nixvim options.
-    # If this fails, then 'programs.nixvim.opts' itself is problematic in your setup.
-    programs.nixvim.opts.number = true; # Display line numbers
-  };
+    plugins = {
+      # ... your other plugins
+      hardtime = {
+        enable = true;
+        # Optional: specify the package if needed, though often NixVim can infer it
+        # package = pkgs.vimPlugins.hardtime-nvim; # Or the correct attribute name
+      };
+    };
 }
